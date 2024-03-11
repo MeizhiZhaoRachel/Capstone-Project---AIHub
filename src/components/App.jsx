f117c880cb218c2c8886abc562fd29e3ef9e3f21
unknown <yuanyiping1122@gmail.com>
2024-03-11 17:00:01 +1100

    xx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import Header from "./Header";
 import SearchBox from "./SearchBox";
 import Categories from "./Categories";
 import Footer from "./Footer";
+import Reviews from "./Reviews";
 
 function App() {
     return (
     <div>
       <Header />
       <SearchBox />
+      <Reviews/>
       <Categories />
       <Footer/>
     </div>
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "../style/Reviews.css";
+
+function Reviews() {
+  return (
+    <div className="reviews-container">
+      <div className="reviews-headerline">Browse reviews</div>
+      <div classname="review1">
+        <div className="review-rating"><span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span></div>
+        <div className="review-reviewer">xxx reviewed chatGPT</div>
+        <hr/>
+        <div className="review-header">Excitement</div>
+          <div className="review-body">I love this excellent AI product, and I got a lot of inspiration from it</div>
+        </div>
+
+        <div classname="review2">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+      <div classname="review3">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+      <div classname="review4">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+      <div classname="review5">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+      <div classname="review6">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+      <div classname="review7">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+      <div classname="review8">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+      <div classname="review9">
+        <div className="review-reviewer"></div>
+        <hr />
+        <div className="review-header"></div>
+        <div className="review-body"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Reviews;

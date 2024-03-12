bac6464282f37fd90e1938b92988feeeddfc1312
unknown <yuanyiping1122@gmail.com>
2024-03-12 16:37:49 +1100

    xx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,7 +5,7 @@ function Reviews() {
   return (
     <div className="reviews-container">
       <div className="reviews-headerline">Browse reviews</div>
-      <div classname="review1">
+      <div className="review1">
         <div className="review-rating"><span>★</span>
           <span>★</span>
           <span>★</span>
@@ -17,49 +17,49 @@ function Reviews() {
           <div className="review-body">I love this excellent AI product, and I got a lot of inspiration from it</div>
         </div>
 
-        <div classname="review2">
+        <div className="review2">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
         <div className="review-body"></div>
       </div>
-      <div classname="review3">
+      <div className="review3">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
         <div className="review-body"></div>
       </div>
-      <div classname="review4">
+      <div className="review4">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
         <div className="review-body"></div>
       </div>
-      <div classname="review5">
+      <div className="review5">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
         <div className="review-body"></div>
       </div>
-      <div classname="review6">
+      <div className="review6">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
         <div className="review-body"></div>
       </div>
-      <div classname="review7">
+      <div className="review7">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
         <div className="review-body"></div>
       </div>
-      <div classname="review8">
+      <div className="review8">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
         <div className="review-body"></div>
       </div>
-      <div classname="review9">
+      <div className="review9">
         <div className="review-reviewer"></div>
         <hr />
         <div className="review-header"></div>
